0834a3d793fc7dcf062c2207ceba8d47e4574ea4
Alexandre Oliveira <Alexandre2001.oliveira@gmail.com>
2023-10-24 15:29:57 -0300

    configuring S3 in the application/

--- src/config/multer.js
+++ src/config/multer.js
@@ -1,14 +1,29 @@
 const multer = require('multer')
+const multerS3 = require('multer-s3')
+const aws = require('aws-sdk')
 const path = require('path')
 const crypto = require('crypto')
 
-module.exports = {
-    dest: path.resolve(__dirname, '..', '..', 'temp', 'uploads'),
-    Storage: multer.diskStorage({
+const StorageTypes = {
+    local:  multer.diskStorage({
         destination: (req, file, cb) =>{
             cb(null, (__dirname, '..', '..', 'temp', 'uploads'))
         },
         filename: (req, file, cb) => {
+            crypto.randomBytes(16, (err, Hash) => {
+                if (err) cb(err);
+                 file.key = `${crypto.Hash.toString('hex')}-${file.originalname}`;
+
+                cb(null, fileName)
+            })
+        }
+    }),
+    s3: multerS3({
+        s3: new aws.S3(),
+        bucket: 'backend-file-uploader',
+        contentType: multerS3.AUTO_CONTENT_TYPE,
+        acl: 'public-read',
+        key: (req, file, cb) =>{
             crypto.randomBytes(16, (err, Hash) => {
                 if (err) cb(err);
                 const fileName = `${crypto.Hash.toString('hex')}-${file.originalname}`;
@@ -16,7 +31,12 @@ module.exports = {
                 cb(null, fileName)
             })
         }
-    }),
+    })
+}
+
+module.exports = {
+    dest: path.resolve(__dirname, '..', '..', 'temp', 'uploads'),
+    Storage: StorageTypes['local'],
     limits:{
         fileSize: 2 * 1024 * 1024
     },
