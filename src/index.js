959d51600b1e405bd8bcd7c3eccaef1a2d4d883a
Alexandre Oliveira <Alexandre2001.oliveira@gmail.com>
2023-10-23 21:45:01 -0300

    Created connection database

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
+require('dotenv').config()
+
 const express = require('express')
 const morgan = require("morgan")
+const mongoose = require('mongoose')
 
 const app = express()
 
@@ -9,4 +12,15 @@ app.use(morgan('dev'));
 
 app.use(require("./routes"))
 
-app.listen(3400)
+// Database.setup
+
+const DB_User = process.env.DB_User
+const DB_Password = process.env.DB_Password
+
+mongoose.connect(`mongodb+srv://${DB_User}:${DB_Password}@backend-file-uploader-c.4m3mepj.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp`, {
+    useNewUrlParser: true
+}).then(() =>{
+    app.listen(3400);
+    console.log('Server is running and connect database')
+}).catch((err) => console.log(err))
+
